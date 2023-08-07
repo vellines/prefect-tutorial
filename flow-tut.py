eececa61d0500b92cd9fcc13b46b89fc0f6ceb78
Michael Vellines <michael@nzero.com>
2023-08-07 15:32:29 -0700

    feat: flow tutorial

---
+++ flow-tut.py
@@ -0,0 +1,18 @@
+import httpx
+from prefect import flow, get_run_logger
+
+
+@flow(retries=3, retry_delay_seconds=5)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
+    response = httpx.get(url)
+    response.raise_for_status()
+    repo = response.json()
+    logger = get_run_logger()
+    logger.info(f"{repo_name} repository statistics 🤓:")
+    logger.info(f"Stars 🌠 : {repo['stargazers_count']}")
+    logger.info(f"Forks 🍴 : {repo['forks_count']}")
+
+
+if __name__ == "__main__":
+    get_repo_info()
